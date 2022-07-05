39495560003f9cd2d01268d53d9dd3f81d8e173e
BurakOztopuz <boztopuz@gmail.com>
2022-07-05 14:54:53 +0300

    Initial Commit

---
+++ SwiftUICryptoCrayz/SwiftUICryptoCrayzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoCrayzApp.swift
+//  SwiftUICryptoCrayz
+//
+//  Created by Burak Öztopuz on 5.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoCrayzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
